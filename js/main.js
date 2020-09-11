857d199f29ea783acd446068decf4a4f51ab7007
Kolya0909 <70721237+Kolya0909@users.noreply.github.com>
2020-09-11 23:25:36 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        
+    });
+
+    $('.menu__btn').on('click',function(){
+        $('.menu__list').slideToggle();
+       
+    })
+    
+});
